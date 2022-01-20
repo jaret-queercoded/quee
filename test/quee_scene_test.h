aaa6acd76a182f4a91cb6fe651f3cdef39784ce6
Jaret Screws <jaret@queercoded.io>
2022-01-19 19:09:25 -0500

    header guards

--- test/quee_scene_test.h
+++ test/quee_scene_test.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef QUEE_SCENE_TEST_H_
+#define QUEE_SCENE_TEST_H_
 
 void test_quee_scene_manager_creation();
 void test_quee_scene_manager_add_scene();
 void test_quee_scene_manager_remove_scene();
+
+#endif
