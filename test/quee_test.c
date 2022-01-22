2fec8148fce22002ec63647c3ddb14649362b33a
Jaret Screws <jaret@queercoded.io>
2022-01-21 23:06:38 -0500

    Trying to see if creating an sdl2 instance will run on github ci

--- test/quee_test.c
+++ test/quee_test.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <SDL.h>
+
 #include "quee_scene_test.h"
+#include "quee_texture_test.h"
+
+#include "../quee_helpers.h"
 
 typedef struct quee_test quee_test;
 
@@ -11,11 +16,12 @@ struct quee_test {
 };
 
 quee_test tests[] = {
-    {.func_ptr = test_quee_scene_manager_creation, .suite = "quee_scene_manager"},
-    {.func_ptr = test_quee_scene_manager_add_scene, .suite = "quee_scene_manager"},
-    {.func_ptr = test_quee_scene_manager_remove_scene, .suite = "quee_scene_manager"},
-    {.func_ptr = test_quee_scene_creation, .suite = "quee_scene"},
-    {.func_ptr = test_quee_load_scene, .suite = "quee_scene"},
+    {.func_ptr = test_quee_scene_manager_creation, .suite = QUEE_SCENE_MANAGER_SUITE},
+    {.func_ptr = test_quee_scene_manager_add_scene, .suite = QUEE_SCENE_MANAGER_SUITE},
+    {.func_ptr = test_quee_scene_manager_remove_scene, .suite = QUEE_SCENE_MANAGER_SUITE},
+    {.func_ptr = test_quee_scene_creation, .suite = QUEE_SCENE_SUITE},
+    {.func_ptr = test_quee_load_scene, .suite = QUEE_SCENE_SUITE},
+    {.func_ptr = test_quee_texture_creation_and_deletion, .suite = QUEE_TEXTURE_SUITE},
 };
 
 //We use a global render pointer in c so I just make a null one here for tests
@@ -23,6 +29,14 @@ typedef struct SDL_Renderer SDL_Renderer;
 SDL_Renderer *g_renderer = NULL;
 
 int main(int argc, char** argv) {
+    check_sdl_code(SDL_Init(SDL_INIT_VIDEO));
+
+    SDL_Window *window = check_sdl_ptr(
+        SDL_CreateWindow("quee", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_HIDDEN)
+    );
+
+    g_renderer =
+        check_sdl_ptr(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
     // Figure out how many tests are in the array of tests
     int num_tests = sizeof(tests) / sizeof(quee_test);
     if(argc == 1) {
@@ -49,5 +63,10 @@ int main(int argc, char** argv) {
             filtered_tests[i].func_ptr();
         }
     }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(g_renderer);
+    SDL_Quit();
+
     return 0;
 }
