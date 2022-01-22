924e9e7917f1d1b872229d5f34305ccdc5f4721a
Jaret Screws <jaret@queercoded.io>
2022-01-21 22:46:10 -0500

    Got rid of the need for defined number of tests and instead compute it from the size of the array and the size of the test structure

--- test/quee_test.c
+++ test/quee_test.c
@@ -3,8 +3,6 @@
 
 #include "quee_scene_test.h"
 
-#define NUMBER_OF_TESTS 5 
-
 typedef struct quee_test quee_test;
 
 struct quee_test {
@@ -12,7 +10,7 @@ struct quee_test {
     char *suite;
 };
 
-quee_test tests[NUMBER_OF_TESTS] = {
+quee_test tests[] = {
     {.func_ptr = test_quee_scene_manager_creation, .suite = "quee_scene_manager"},
     {.func_ptr = test_quee_scene_manager_add_scene, .suite = "quee_scene_manager"},
     {.func_ptr = test_quee_scene_manager_remove_scene, .suite = "quee_scene_manager"},
@@ -25,19 +23,21 @@ typedef struct SDL_Renderer SDL_Renderer;
 SDL_Renderer *g_renderer = NULL;
 
 int main(int argc, char** argv) {
+    // Figure out how many tests are in the array of tests
+    int num_tests = sizeof(tests) / sizeof(quee_test);
     if(argc == 1) {
-        for(int i = 0; i < NUMBER_OF_TESTS; i++) {
-            printf("Running test [%d/%d]: ", i + 1, NUMBER_OF_TESTS);
+        for(int i = 0; i < num_tests; i++) {
+            printf("Running test [%d/%d]: ", i + 1, num_tests);
             tests[i].func_ptr();
         }
     }
     // Selective Test Running
     else {
-        quee_test filtered_tests[NUMBER_OF_TESTS];
+        quee_test filtered_tests[num_tests];
         int x = 0;
         for(int i = 1; i < argc; i++) {
             char* target = argv[i];
-            for(int j = 0; j < NUMBER_OF_TESTS; j++) {
+            for(int j = 0; j < num_tests; j++) {
                 if(strcmp(target, tests[j].suite) == 0) {
                     filtered_tests[x++] = tests[j];
                 }
