fa3d0337f40153042f1ccf82c44cecb3f994cccc
Jaret Screws <jaret@queercoded.io>
2022-01-22 16:38:54 -0500

    Fixed sprite ut

--- test/quee_sprite_test.c
+++ test/quee_sprite_test.c
@@ -6,7 +6,7 @@
 
 quee_test_result test_quee_sprite_creation() {
     quee_texture_manager *manager = create_quee_texture_manager(1);
-    quee_sprite *sprite = create_quee_sprite(get_quee_texture_from_texture_manager(manager, "assets/test/test.png"));
+    quee_sprite *sprite = create_quee_sprite(get_quee_texture_from_texture_manager(manager, "assets/test/test.png"), (vec2i){0,0}, (vec2i){0,0});
     QUEE_ASSERT(sprite, "We didn't get a sprite back");
     destroy_quee_sprite(&sprite);
     QUEE_ASSERT(sprite == NULL, "Sprite destroy left a dangling pointer");
