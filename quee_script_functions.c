59da7819a03cb6e78de910ad9907dbc8ed5f0edd
Jaret Screws <jaret@queercoded.io>
2022-02-07 11:11:33 -0500

    lua null ptr checking

--- quee_script_functions.c
+++ quee_script_functions.c
@@ -2,11 +2,26 @@
 #include "lua.h"
 #include "quee_entity.h"
 
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+
+inline void * check_lua_ptr_func(void *ptr, char *func)
+{
+    if(ptr == NULL) {
+        fprintf(stderr, "quee got a null ptr from lua in %s\nPress Enter to continue...\n", func);
+        getchar();
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+#define check_lua_ptr(ptr) check_lua_ptr_func(ptr, __func__);
 
 int quee_script_get_pos(lua_State *L) {
-    quee_entity *test = (quee_entity *)lua_touserdata(L, 1);
+    quee_entity *test = check_lua_ptr(lua_touserdata(L, 1));
+    printf("%p\n", test);
     printf("%s\n", test->name);
     return 0;
 }
