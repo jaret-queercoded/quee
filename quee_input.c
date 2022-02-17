9347ff5ff820d3ea7c70027b62a276f5c53bbcc5
Jaret Screws <jaret@queercoded.io>
2022-02-16 19:57:43 -0500

    Temporary crash fix

--- quee_input.c
+++ quee_input.c
@@ -15,11 +15,17 @@ void destroy_quee_input_manager(quee_input_manager **manager) {
     *manager = NULL;
 }
 
+//TODO pressing capslock and many other keys that arent letters
+//causes a boundary error and crashes the program
 void quee_input_pressed(quee_input_manager *manager, int code) {
     printf("Pressed key: %d\n", code);
+    //TODO Temporary fix to stop crash
+    if(code > 200) return;
     manager->key_pressed[code] = true;
 }
 
 void quee_input_released(quee_input_manager *manager, int code) {
+    //TODO Temporary fix to stop crash
+    if(code > 200) return;
     manager->key_pressed[code] = false;
 }
